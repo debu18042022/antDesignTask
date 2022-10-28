52fbc33ee8f11068c0eeb0a95ebed244f951a269
sujeetsahu-cedcommerce <sujeetsahu@cedcoss.com>
2022-10-28 11:17:06 +0530

    Expanded type Ant Grid

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import { Table } from "antd";
 import React, { useEffect, useState } from "react";
 import "antd/dist/antd.css";
-const columns = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-  },
-  {
-    title: "phone",
-    dataIndex: "phone",
-    key: "phone",
-  },
-  {
-    title: "Address",
-    dataIndex: "address",
-    key: "address",
-  },
-  {
-    title: "Action",
-    dataIndex: "",
-    key: "x",
-    render: () => <a>Delete</a>,
-  },
-];
-
 
 const App = () => {
   const [users, setUsers] = useState([]);
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "phone",
+      dataIndex: "phone",
+      key: "phone",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      key: "x",
+      
+    },
+  ];
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
@@ -36,18 +35,35 @@ const App = () => {
       });
   }, []);
 
-  let data = users && users.map((i) => {
+  let data =
+    users &&
+    users.map((i, index) => {
       return {
         key: i.id,
         name: i.name,
         phone: i.phone,
         address: i.address.city,
         description: [i.company.name, i.company.catchPhrase, i.company.bs],
+        action: <button onClick={(e) => RemoveUsers(index)}>Delete</button>,
       };
     });
 
+  const RemoveUsers = (index) => {
+    alert(index);
+    let tempUsers = [...users];
+    tempUsers.splice(index, 1);
+    setUsers(tempUsers);
+  };
+  console.log("data", data);
+
   return (
     <Table
+      expandable={{
+        expandedRowRender: (record) => (
+          <p style={{ margin: 0 }}>{record.description}</p>
+        ),
+        rowExpandable: (record) => record.name !== "Not Expandable",
+      }}
       columns={columns}
       dataSource={data}
     />
